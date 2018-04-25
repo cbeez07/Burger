98a2d1a2bbe62a72dd8f28b896e9dd6fb1056285
Chris B <beesleychc@gmail.com>
2018-04-25 18:52:04 -0500

    create routes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggieburger', false);
