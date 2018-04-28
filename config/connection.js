577cb19b92de210fe8cbab8b63427c29e0fe12b7
Chris B <beesleychc@gmail.com>
2018-04-28 10:40:21 -0500

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burger_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burger_db'
+    });
+}
 
 connection.connect((err) => {
     if (err) {
